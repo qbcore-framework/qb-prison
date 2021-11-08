c5b71cf0353be4f09d912a6efdce75d60897386c
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-11-08 02:04:58 +0200

    refactor event

--- server/main.lua
+++ server/main.lua
@@ -2,8 +2,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 local AlarmActivated = false
 
-RegisterServerEvent('prison:server:SetJailStatus')
-AddEventHandler('prison:server:SetJailStatus', function(jailTime)
+RegisterNetEvent('prison:server:SetJailStatus', function(jailTime)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     Player.Functions.SetMetaData("injail", jailTime)
@@ -15,32 +14,29 @@ AddEventHandler('prison:server:SetJailStatus', function(jailTime)
     end
 end)
 
-RegisterServerEvent('prison:server:SaveJailItems')
-AddEventHandler('prison:server:SaveJailItems', function()
+RegisterNetEvent('prison:server:SaveJailItems', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if Player.PlayerData.metadata["jailitems"] == nil or next(Player.PlayerData.metadata["jailitems"]) == nil then
         Player.Functions.SetMetaData("jailitems", Player.PlayerData.items)
         Player.Functions.AddMoney('cash', 80)
-        Citizen.Wait(2000)
+        Wait(2000)
         Player.Functions.ClearInventory()
     end
 end)
 
-RegisterServerEvent('prison:server:GiveJailItems')
-AddEventHandler('prison:server:GiveJailItems', function()
+RegisterNetEvent('prison:server:GiveJailItems', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Citizen.Wait(1000)
+    Wait(1000)
     for k, v in pairs(Player.PlayerData.metadata["jailitems"]) do
         Player.Functions.AddItem(v.name, v.amount, false, v.info)
     end
-    Citizen.Wait(1000)
+    Wait(1000)
     Player.Functions.SetMetaData("jailitems", {})
 end)
 
-RegisterServerEvent('prison:server:SecurityLockdown')
-AddEventHandler('prison:server:SecurityLockdown', function()
+RegisterNetEvent('prison:server:SecurityLockdown', function()
     TriggerClientEvent("prison:client:SetLockDown", -1, true)
     for k, v in pairs(QBCore.Functions.GetPlayers()) do
         local Player = QBCore.Functions.GetPlayer(v)
@@ -52,8 +48,7 @@ AddEventHandler('prison:server:SecurityLockdown', function()
 	end
 end)
 
-RegisterServerEvent('prison:server:SetGateHit')
-AddEventHandler('prison:server:SetGateHit', function(key)
+RegisterNetEvent('prison:server:SetGateHit', function(key)
     TriggerClientEvent("prison:client:SetGateHit", -1, key, true)
     if math.random(1, 100) <= 50 then
         for k, v in pairs(QBCore.Functions.GetPlayers()) do
@@ -67,8 +62,7 @@ AddEventHandler('prison:server:SetGateHit', function(key)
     end
 end)
 
-RegisterServerEvent('prison:server:CheckRecordStatus')
-AddEventHandler('prison:server:CheckRecordStatus', function()
+RegisterNetEvent('prison:server:CheckRecordStatus', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local CriminalRecord = Player.PlayerData.metadata["criminalrecord"]
@@ -86,8 +80,7 @@ AddEventHandler('prison:server:CheckRecordStatus', function()
     end
 end)
 
-RegisterServerEvent('prison:server:JailAlarm')
-AddEventHandler('prison:server:JailAlarm', function()
+RegisterNetEvent('prison:server:JailAlarm', function()
     if not AlarmActivated then
         TriggerClientEvent('prison:client:JailAlarm', -1, true)
         SetTimeout(5 * (60 * 1000), function()
