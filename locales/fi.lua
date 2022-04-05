12fc7418583fcc2e67fbf23f53c385d7f6f741dd
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-04-05 17:24:03 +0200

    feat(locales/fi): add new phrase

--- locales/fi.lua
+++ locales/fi.lua
@@ -20,8 +20,10 @@ local Translations = {
     info = {
         ["timeleft"] = "Tuomiosi kestää vielä %{JAILTIME} kuukautta",
         ["lost_job"] = "Olet saaut potkut",
+        ["job_interaction"] = "[E] Electricity Work", -- This needs to be translated
     }
 }
 Lang = Locale:new({
-phrases = Translations,
-warnOnMissing = true})
+    phrases = Translations,
+    warnOnMissing = true
+})
