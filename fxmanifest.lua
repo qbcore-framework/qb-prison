20b57cbdb62763204ca0a7e7d19864bfe4c85c33
Dahamster <6582577+blastboy@users.noreply.github.com>
2024-02-13 11:01:21 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-use_fxv2_oal 'yes'
+use_experimental_fxv2_oal 'yes'
 author 'Kakarot'
 description 'Allows players to be jailed, escape from jail and work in jail'
 version '2.1.0'
