bda0412206a3d83a8f7bb2f56fb3825cdbeec31a
Marcell <chillcode.codersparadise@gmail.com>
2024-09-12 16:44:48 +0200

    added missing variables and fixed spelling errors

--- locales/de.lua
+++ locales/de.lua
@@ -1,27 +1,43 @@
 local Translations = {
     error = {
-        ["missing_something"] = "Es sieht dir aus wie als würde dir was Fehlen...",
+        ["missing_something"] = "Es scheint, als würde dir etwas fehlen...",
         ["not_enough_police"] = "Nicht genug Polizisten..",
-        ["door_open"] = "Die Türe ist bereits offen..",
-        ["cancelled"] = "Prozess abgebrochen..",
-        ["didnt_work"] = "Es hat nicht Funktioniert..",
-        ["emty_box"] = "Die Box ist leer..",
-        ["injail"] = "Du bist im Gefängnis für %{Time} weitere Monate..",
-        ["item_missing"] = "Dir fehlt ein Gegenstand..",
+        ["door_open"] = "Die Tür ist bereits offen...",
+        ["cancelled"] = "Prozess abgebrochen...",
+        ["didnt_work"] = "Es hat nicht funktioniert...",
+        ["emty_box"] = "Die Box ist leer...",
+        ["injail"] = "Du bist für %{Time} Monate im Gefängnis...",
+        ["item_missing"] = "Dir fehlt ein Gegenstand...",
         ["escaped"] = "Du bist ausgebrochen, Schnell weg!",
         ["do_some_work"] = "Mache etwas Arbeit um deine Haftzeit zu verkürzen, Job: %{currentjob} ",
+        ["security_activated"] = "Das höchste Sicherheitsniveau ist aktiv, bleib in den Zellenblöcken!"
     },
     success = {
-        ["found_phone"] = "Du findest ein Handy..",
-        ["time_cut"] = "Du hast deine Haftzeit verkürzt.",
-        ["free_"] = "Du bist frei, Viel Spaß :)",
+        ["found_phone"] = "Du hast ein Telefon gefunden...",
+        ["time_cut"] = "Du hast deine Strafe durch Arbeit verkürzt.",
+        ["free_"] = "Du bist frei! Genieß es! :)",
         ["timesup"] = "Deine Zeit ist Vorbei, Gehe zum Besucher Zentrum",
     },
     info = {
-        ["timeleft"] = "Du hast noch %{JAILTIME} Monate",
+        ["timeleft"] = "Du musst noch %{JAILTIME} Monate absitzen",
         ["lost_job"] = "Du hast deine Job verloren und bist jetzt Arbeitslos",
+        ["job_interaction"] = "[E] Elektrizitätsarbeit",
+        ["job_interaction_target"] = "Mache %{job}-Arbeit",
+        ["received_property"] = "Du hast dein Eigentum zurückbekommen...",
+        ["seized_property"] = "Dein Eigentum wurde beschlagnahmt, du bekommst alles zurück, wenn deine Zeit abgelaufen ist...",
+        ["cells_blip"] = "Zellen",
+        ["freedom_blip"] = "Gefängnis-Empfang",
+        ["canteen_blip"] = "Kantine",
+        ["work_blip"] = "Gefängnisarbeit",
+        ["target_freedom_option"] = "Zeit überprüfen",
+        ["target_canteen_option"] = "Essen holen",
+        ["police_alert_title"] = "Neuer Anruf",
+        ["police_alert_description"] = "Gefängnisausbruch",
+        ["connecting_device"] = "Gerät verbinden",
+        ["working_electricity"] = "Drähte verbinden"
     }
 }
+
 
 if GetConvar('qb_locale', 'en') == 'de' then
     Lang = Locale:new({
